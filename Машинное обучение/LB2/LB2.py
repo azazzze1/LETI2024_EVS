f1b44219390a7395109b41be5d31a4e59b895e6b
azazzze1 <90767835+azazzze1@users.noreply.github.com>
2024-04-16 13:54:13 +0300

    lb2: task 4 - done

--- Машинное обучение/LB2/LB2.py
+++ Машинное обучение/LB2/LB2.py
@@ -3,42 +3,44 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 from sklearn.metrics import silhouette_score
+from sklearn.neighbors import NearestNeighbors
 from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 # Вариант 134Б 
 
 # ЗАДАНИЕ 1 ---------------------------------------------------------
 
-dFrameBlobs = pd.read_csv("data\lab2_blobs.csv")
-dFrameChecker = pd.read_csv("data\lab2_checker.csv")
-dFrameMoons = pd.read_csv("data\lab2_noisymoons.csv")
+# dFrameBlobs = pd.read_csv("data\lab2_blobs.csv")
+# dFrameChecker = pd.read_csv("data\lab2_checker.csv")
+# dFrameMoons = pd.read_csv("data\lab2_noisymoons.csv")
 
-print(dFrameBlobs.describe())
+# print(dFrameBlobs.describe())
 
 # print(dFrameBlobs.head())
 # print(dFrameChecker.head())
 # print(dFrameMoons.head())
 
-fig, axs = plt.subplots(1, 3, sharex='col')
+# fig, axs = plt.subplots(1, 3, sharex='col')
 
 # sns.scatterplot(dFrameBlobs, ax = axs[0], x = "# x", y = "y").set(title="Blobs")
 # sns.scatterplot(dFrameChecker, ax = axs[1], x = "# x", y = "y").set(title="Checker")
 # sns.scatterplot(dFrameMoons, ax = axs[2], x = "# x", y = "y").set(title="Moons") 
 
 # scaler = MinMaxScaler()
-scaler = StandardScaler()
+# scaler = StandardScaler()
 
 
-scaler.fit(dFrameBlobs) 
-dFrameBlobsScale = scaler.transform(dFrameBlobs)
+# scaler.fit(dFrameBlobs) 
+# dFrameBlobsScale = scaler.transform(dFrameBlobs)
 
-scaler.fit(dFrameChecker) 
-dFrameCheckerScale = scaler.transform(dFrameChecker)
+# scaler.fit(dFrameChecker) 
+# dFrameCheckerScale = scaler.transform(dFrameChecker)
 
-scaler.fit(dFrameMoons) 
-dFrameMoonsScale = scaler.transform(dFrameMoons)
+# scaler.fit(dFrameMoons) 
+# dFrameMoonsScale = scaler.transform(dFrameMoons)
 
 # sns.scatterplot(pd.DataFrame(dFrameBlobsScale, columns=["# x", "y"]), ax = axs[0], x = "# x", y = "y").set(title="Blobs")
 # sns.scatterplot(pd.DataFrame(dFrameCheckerScale, columns=["# x", "y"]), ax = axs[1], x = "# x", y = "y").set(title="Checker")
@@ -84,34 +86,34 @@ dFrameMoonsScale = scaler.transform(dFrameMoons)
 
 ## Задание 2.3 ----------------
 
-kmeansBlobs = KMeans(n_clusters = 5, 
-			 init = 'random',
-			 n_init = 10)
+# kmeansBlobs = KMeans(n_clusters = 5, 
+# 			 init = 'random',
+# 			 n_init = 10)
 
-clustIndexBlobs = kmeansBlobs.fit_predict(dFrameBlobsScale)
-centersBlobs = kmeansBlobs.cluster_centers_
+# clustIndexBlobs = kmeansBlobs.fit_predict(dFrameBlobsScale)
+# centersBlobs = kmeansBlobs.cluster_centers_
 # print("clust_index Blobs: ", clustIndexBlobs)
 # print("censters Blobs: ", centersBlobs)
 # print("Blobs inetrion: ", kmeansBlobs.inertia_) 
 
 
-kmeansChecker = KMeans(n_clusters = 5, 
-			 init = 'random',
-			 n_init = 10)
+# kmeansChecker = KMeans(n_clusters = 5, 
+# 			 init = 'random',
+# 			 n_init = 10)
 
-clustIndexChecker = kmeansChecker.fit_predict(dFrameCheckerScale)
-centersChecker = kmeansChecker.cluster_centers_
+# clustIndexChecker = kmeansChecker.fit_predict(dFrameCheckerScale)
+# centersChecker = kmeansChecker.cluster_centers_
 # print("clust_index Checker: ", clustIndexChecker)
 # print("censters Checker: ", centersChecker)
 # print("Checker inetrion: ", kmeansChecker.inertia_) 
 
 
-kmeansMoons = KMeans(n_clusters = 10, 
-			 init = 'random',
-			 n_init = 10)
+# kmeansMoons = KMeans(n_clusters = 10, 
+# 			 init = 'random',
+# 			 n_init = 10)
 
-clustIndexMoons = kmeansMoons.fit_predict(dFrameMoonsScale)
-centersMoons = kmeansMoons.cluster_centers_
+# clustIndexMoons = kmeansMoons.fit_predict(dFrameMoonsScale)
+# centersMoons = kmeansMoons.cluster_centers_
 # print("clust_index Moons: ", clustIndexMoons)
 # print("censters Moons: ", centersMoons)
 # print("Moons inetrion: ", kmeansMoons.inertia_) 
@@ -119,9 +121,9 @@ centersMoons = kmeansMoons.cluster_centers_
  
 ## Задание 2.4 ----------------
 
-dFrameBlobsKmeans = pd.DataFrame(dFrameBlobsScale, columns=["# x", "y"]).assign(cluster=clustIndexBlobs)
-dFrameCheckerKmeans = pd.DataFrame(dFrameCheckerScale, columns=["# x", "y"]).assign(cluster=clustIndexChecker)
-dFrameMoonsKmeans = pd.DataFrame(dFrameMoonsScale, columns=["# x", "y"]).assign(cluster=clustIndexMoons)
+# dFrameBlobsKmeans = pd.DataFrame(dFrameBlobsScale, columns=["# x", "y"]).assign(cluster=clustIndexBlobs)
+# dFrameCheckerKmeans = pd.DataFrame(dFrameCheckerScale, columns=["# x", "y"]).assign(cluster=clustIndexChecker)
+# dFrameMoonsKmeans = pd.DataFrame(dFrameMoonsScale, columns=["# x", "y"]).assign(cluster=clustIndexMoons)
 
 # sns.scatterplot(dFrameBlobsKmeans, ax = axs[0], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Blobs")
 # sns.scatterplot(dFrameCheckerKmeans, ax = axs[1], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Checker")
@@ -199,3 +201,85 @@ dFrameMoonsKmeans = pd.DataFrame(dFrameMoonsScale, columns=["# x", "y"]).assign(
 
 
 ## ЗАДАНИЕ 3 ---------------------------------------------------------
+
+# def kDistanceGraph(dFrame, dFrameTitle, axIdx):
+#     neigh = NearestNeighbors(n_neighbors = 2)
+#     nbrs = neigh.fit(dFrame)
+#     distances, indices = nbrs.kneighbors(dFrame)
+#     distances = (np.sort(distances, axis=0))[:, 1]
+#     sns.lineplot(distances, ax=axs[axIdx]).set(title=dFrameTitle, ylabel = "Epsilon", xlabel = "Points sorted by distance to neighboor")
+    
+
+# kDistanceGraph(dFrameBlobsScale, "Blobs", 0) # 0.09 - 0.15
+# kDistanceGraph(dFrameCheckerScale, "Checker", 1) # 0.10
+# kDistanceGraph(dFrameMoonsScale, "Moons", 2) # 0.15 
+
+# dbs = DBSCAN(eps=0.15, min_samples=5)
+# clustersBlobs = dbs.fit_predict(dFrameBlobsScale)
+# dFrameBlobsKmeans = pd.DataFrame(dFrameBlobsScale, columns=["# x", "y"]).assign(cluster=clustersBlobs)
+# sns.scatterplot(dFrameBlobsKmeans, ax = axs[0], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Blobs")
+
+# dbs = DBSCAN(eps=0.17, min_samples=6)
+# clustersChecker = dbs.fit_predict(dFrameCheckerScale)
+# dFrameCheckerKmeans = pd.DataFrame(dFrameCheckerScale, columns=["# x", "y"]).assign(cluster=clustersChecker)
+# sns.scatterplot(dFrameCheckerKmeans, ax = axs[1], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Checker")
+
+# dbs = DBSCAN(eps=0.20, min_samples=5)
+# clustersMoons = dbs.fit_predict(dFrameMoonsScale)
+# dFrameMoonsKmeans = pd.DataFrame(dFrameMoonsScale, columns=["# x", "y"]).assign(cluster=clustersMoons)
+# sns.scatterplot(dFrameMoonsKmeans, ax = axs[2], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Moons")
+
+# plt.show()
+
+
+## ЗАДАНИЕ 4 ---------------------------------------------------------
+
+# axs[0].set(title="Blobs")
+# axs[1].set(title="Checker")
+# axs[2].set(title="Moons")
+
+# _linkage = "average"
+
+# agc = AgglomerativeClustering(5, linkage = _linkage)
+# ag_clust = agc.fit_predict(dFrameBlobsScale)
+# dFrameBlobsAgc = pd.DataFrame(dFrameBlobsScale, columns=["# x", "y"]).assign(cluster=ag_clust)
+# dendrogram(linkage(dFrameBlobsAgc, method=_linkage), ax = axs[0])
+# sns.scatterplot(dFrameBlobsAgc, ax = axs[0], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Blobs")
+
+# agc = AgglomerativeClustering(5, linkage = _linkage)
+# ag_clust = agc.fit_predict(dFrameCheckerScale)
+# dFrameCheckerAgc = pd.DataFrame(dFrameCheckerScale, columns=["# x", "y"]).assign(cluster=ag_clust)
+# dendrogram(linkage(dFrameCheckerAgc, method=_linkage), ax = axs[1])
+# sns.scatterplot(dFrameCheckerAgc, ax = axs[1], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Checker")
+
+# agc = AgglomerativeClustering(7, linkage = _linkage)
+# ag_clust = agc.fit_predict(dFrameMoonsScale)
+# dFrameMoonsAgc = pd.DataFrame(dFrameMoonsScale, columns=["# x", "y"]).assign(cluster=ag_clust)
+# dendrogram(linkage(dFrameMoonsAgc, method=_linkage))
+# sns.scatterplot(dFrameMoonsAgc, ax = axs[2], x = "# x", y = "y", hue="cluster", palette="Set1").set(title="Moons")
+
+# plt.show()
+
+
+## ЗАДАНИЕ 5 ---------------------------------------------------------
+
+dFrameWine = pd.read_csv("data\lab2_winequality_white.csv", sep=";")
+
+# print(dFrameWine.head())
+
+print(dFrameWine.describe())
+print(dFrameWine.columns.to_list())
+
+fig, axs = plt.subplots(4, 3, sharex='col')
+
+axIdx = 0
+for column in dFrameWine.columns.to_list(): 
+    sns.histplot(dFrameWine, ax = axs[axIdx], bins=10, x = column, hue="quality", palette="Set1")
+    axIdx+=1
+
+## 
+
+# print(dFrameWine.head()) 
+
+
+
